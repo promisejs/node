56d1b0c1368d0779e16ebdab7216c000c628e811
Joanna Galewska <galewska.joanna@gmail.com>
2017-04-26 22:27:54 +0200

    Setting mocha & chai

--- test/test.js
+++ test/test.js
@@ -1,1 +1,26 @@
-ECHO is on.
+let assert = require('assert');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+const app = require('../server');
+const events = require('../data')
+
+chai.use(chaiHttp);
+
+describe('TEST API', () => {
+
+  beforeEach((done) => {
+    //remove all events from json?
+  })
+
+  describe('/GET events', () => {
+    it('It should get all the events', (done) => {
+      chai.request('http://localhost:3000')
+      .get('/events')
+      .end((err, res) => {
+        res.should.have.status(200)
+        //to finish
+      })
+    })
+  })
+})
